2f920dbed810c04850a2bd4415193fb3b413842f
Brian Chu <brianchu14@gmail.com>
2019-07-11 10:01:40 -0700

    finished second feature branch

--- client/assets/js/main.js
+++ client/assets/js/main.js
@@ -11,12 +11,14 @@ function initializeApp() {
 }
 
 function applyClickHandlers() {
+    
     $('#number-block').on('click', '.number', numberButtonHandler);
     $('#operator-column').on('click', '.operator', operatorButtonHandler);
     $('#equals').click(equalsButtonHandler);
 }
 
 function numberButtonHandler(event) {
+    console.log("calculationArray number", calculationArray);
     // console.log(event);
     var inputtedNumber = '';
     inputtedNumber = event.currentTarget;
@@ -24,34 +26,71 @@ function numberButtonHandler(event) {
 
     stringNumberToPush = stringNumberToPush.concat(inputtedNumber);
     displayArray.push(inputtedNumber);
-    // console.log(displayArray);
     updateDisplay();
 }
 
 
 function operatorButtonHandler(event) {
-    // console.log(event);
+    console.log("calculationArray operator", calculationArray);
+
     var inputtedOperator = '';
     inputtedOperator = $(event.currentTarget).find('p').text();
     displayArray.push(inputtedOperator);
     updateDisplay();
 
     calculationArray.push(stringNumberToPush);
-    console.log(calculationArray);
+    calculationArray.push(inputtedOperator);
     stringNumberToPush = '';
 }
 
 function equalsButtonHandler(event) {
-    // console.log(event);
+    console.log("calculationArray equal", calculationArray);
+
     calculationArray.push(stringNumberToPush);
     stringNumberToPush = '';
     displayArray = [];
 
-    console.log(calculationArray);
+    console.log("calculation array:", calculationArray);
+    var answer = calculate(calculationArray[0], calculationArray[2], calculationArray[1]);
+    calculationArray = [];
+    stringNumberToPush = answer.toString();
+    console.log(answer);
+
+    displayArray.push(answer);
+    updateDisplay();
 }
 
 function updateDisplay() {
-    var displayText = displayArray.join("");
+    var displayText = displayArray.join("")
+    // var displayText = Number(displayText).toFixed(10);
     // console.log(displayText);
     $('#display-text').text(displayText);
 }
+
+function calculate(num1, num2, operator) {
+    var number1 = parseFloat(num1);
+    var number2 = parseFloat(num2);
+    console.log(number1, number2);
+    var result = null;
+
+    switch(operator) {
+        case '+':
+            result = number1 + number2;
+            break;
+        case '-':
+            result = number1 - number2;
+            break;
+        case '/':
+            result = number1 / number2;
+            break;
+        case '*':
+            result = number1 * number2;
+            break;
+        default:
+            break;
+    }
+    return result;
+    
+}
+
+console.log(1.0, 2, '*');
